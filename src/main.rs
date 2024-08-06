0aaf8a6749a0e0a3f81a660205c5d9664c33291c
lyuts <dioxinu@gmail.com>
2024-08-05 23:58:57 -0400

    Refactor function to return results instead of printing.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
+use anyhow::{self, Context};
+use std::collections::HashMap;
 use tree_sitter::{Node, Parser, TreeCursor};
 
-fn main() {
+fn main() -> anyhow::Result<()> {
     let mut parser = Parser::new();
     let language = tree_sitter_rust::language();
-    parser.set_language(&language);
+    parser
+        .set_language(&language)
+        .context("Failed to load Rust grammar")?;
 
     let code = r#"
     fn example() {
@@ -12,7 +16,9 @@ fn main() {
     }
     "#;
 
-    let tree = parser.parse(code, None).unwrap();
+    let tree = parser
+        .parse(code, None)
+        .context("Failed to parse source code")?;
     let mut cursor = tree.walk();
 
     if cursor.node().kind() == "source_file" {
@@ -23,27 +29,57 @@ fn main() {
     let node_type = node.kind();
     assert_eq!(node_type, "function_item");
 
-    find_function_calls(&mut cursor, code);
-}
-
-fn find_function_calls(cursor: &mut TreeCursor, code: &str) {
-    cursor.goto_first_child();
-    let starting_node = cursor.node();
-    let level = 0;
-    while cursor.goto_next_sibling() {
-        inspect(cursor.node(), code, level);
+    let mut function_name = "".to_string();
+    if let Some(function_name_node) = node.child_by_field_name("name") {
+        function_name = code[function_name_node.byte_range()].to_string();
+        println!("Function call graph belongs to {}", function_name);
     }
+
+    cursor.goto_first_child();
+    let level = 0;
+    let mut calls: HashMap<String, Vec<String>> = HashMap::new();
+    while cursor.goto_next_sibling() {
+        inspect(
+            cursor.node(),
+            code,
+            level,
+            function_name.clone(),
+            &mut calls,
+        );
+    }
+
+    for (k, v) in calls.iter() {
+        for f in v {
+            println!("{} calls {}", k, f);
+        }
+    }
+    Ok(())
 }
 
-fn inspect(node: Node, code: &str, level: usize) {
+fn inspect(
+    node: Node,
+    code: &str,
+    level: usize,
+    caller: String,
+    call_map: &mut HashMap<String, Vec<String>>,
+) {
+    let mut function_name = caller.clone();
     for child in node.children(&mut node.walk()) {
-        inspect(child, code, level + 1);
+        if node.kind() == "function_item" {
+            if let Some(function_name_node) = node.child_by_field_name("name") {
+                function_name = code[function_name_node.byte_range()].to_string();
+            }
+        }
+        inspect(child, code, level + 1, function_name.clone(), call_map);
     }
 
     if node.kind() == "call_expression" {
         if let Some(function_name_node) = node.child_by_field_name("function") {
             let function_name = code[function_name_node.byte_range()].to_string();
-            println!("{}Function call: {}", "\t".repeat(level), function_name);
+            if !call_map.contains_key(&caller) {
+                call_map.insert(caller.clone(), vec![]);
+            }
+            call_map.get_mut(&caller).unwrap().push(function_name);
         }
     }
 }
