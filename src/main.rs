c989e9c0ec9f132d8a713bad543d48f5aa839ff2
lyuts <dioxinu@gmail.com>
2024-08-06 01:03:43 -0400

    Add support for cli args.

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,47 @@
 use anyhow::{self, Context};
-use std::collections::HashMap;
+use clap::{Args, Parser as ClapParser, Subcommand};
+use std::{collections::HashMap, io::Read};
 use tree_sitter::{Node, Parser};
 
+/// Simple program to greet a person
+#[derive(ClapParser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Callgraph(CallgraphArgs),
+}
+
+#[derive(Args)]
+struct CallgraphArgs {
+    filepath: Option<String>,
+}
+
 fn main() -> anyhow::Result<()> {
-    let code = r#"
-    fn example() {
-        obj.foo();
-        let x = bar();
-    }
+    let args = Cli::parse();
 
-    fn f2() {
-        baz(quax(3));
-    }
-    "#;
+    match &args.command {
+        Commands::Callgraph(cmd) => {
+            let mut r: Box<dyn Read>;
+            if let Some(path) = &cmd.filepath { 
+                r = Box::new(std::fs::File::open(path.clone())?);
+            } else {
+                r = Box::new(std::io::stdin());
+            }
+            let mut code = String::new();
+            r.read_to_string(&mut code)?;
 
-    let call_map = find_function_calls(code.to_string())?;
+            let call_map = find_function_calls(code.to_string()).context("Failed to process file.")?;
 
-    for (k, v) in call_map.iter() {
-        for f in v {
-            println!("{} calls {}", k, f);
+            for (k, v) in call_map.iter() {
+                for f in v {
+                    println!("{} calls {}", k, f);
+                }
+            }
         }
     }
     Ok(())
